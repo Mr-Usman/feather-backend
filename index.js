68a3301fbf040cfe23fb5b6918764b7e1c068122
Mr-Usman <usmanrehmat15@outlook.com>
2022-06-30 03:36:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const data = require("./appData");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/policies", function (req, res) {
+  res.status(200).json({ data });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
